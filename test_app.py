c8a74d9a4e928cb3c9006a0a71b7b86371dbcac8
Romi <romi.probal@intradef.gouv.fr>
2026-09-24 14:27:45 +0200

    fix: corriger la fin du test_status_endpoint et ajouter le saut de ligne

--- test_app.py
+++ test_app.py
@@ -20,3 +20,4 @@ def test_status_endpoint():
     client = app.test_client()
     response = client.get("/status")
     assert response.status_code == 200
+    assert response.get_json()["service"] == "projet-devops-groupe-demo"
